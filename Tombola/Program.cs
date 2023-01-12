4793403baca7f40a7f4bc9cc1414c2e293efbd1c
Simone Bassanelli <bassanelli.simone.studente@itispaleocapa.it>
2023-01-12 14:55:18 +0100

    Aggiungere i file di progetto.

---
+++ Tombola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tombola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //stampa tabellone al centro dello schermo
+            int x = Console.WindowWidth / 2;
+            int y = Console.WindowHeight / 2;
+            Console.SetCursorPosition(x, y);
+            tabellone();
+
+            //funzione creazione del tabellone tabellone 
+            void tabellone()
+            {
+                for (int i = 1; i <= 90; i++)
+                {
+                    Console.Write(i + " ");
+                    if (i % 9 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
